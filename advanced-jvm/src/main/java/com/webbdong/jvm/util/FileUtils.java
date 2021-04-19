090f5ff765cd9422e37acab26473e461ecb416f0
Webb Dong <wangwebb1991@gmail.com>
2021-04-19 13:10:33 +0800

    FileUtils

---
+++ advanced-jvm/src/main/java/com/webbdong/jvm/util/FileUtils.java
@@ -0,0 +1,28 @@
+package com.webbdong.jvm.util;
+
+import java.io.File;
+
+/**
+ * @author Webb Dong
+ * @date 2021-04-18 11:05 PM
+ */
+public class FileUtils {
+
+    private FileUtils() {}
+
+    public static long getLastModifiedTime(String path) {
+        if (path == null || "".equals(path)) {
+            return -1;
+        }
+        File file = new File(path);
+        return getLastModifiedTime(file);
+    }
+
+    public static long getLastModifiedTime(File file) {
+        if (!file.exists() || file.isDirectory()) {
+            return -1;
+        }
+        return file.lastModified();
+    }
+
+}
