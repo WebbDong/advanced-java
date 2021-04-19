b74feb7510cdd384def06ced14c0f5a4bbb38c47
Webb Dong <wangwebb1991@gmail.com>
2021-04-19 13:10:43 +0800

    TestClass

---
+++ advanced-jvm/src/main/java/com/webbdong/jvm/hotloadandunload/TestClass.java
@@ -0,0 +1,14 @@
+package com.webbdong.jvm.hotloadandunload;
+
+/**
+ * 测试类
+ * @author Webb Dong
+ * @date 2021-04-18 4:48 PM
+ */
+public class TestClass {
+
+    public void run() {
+        System.out.println("Hello World 1000");
+    }
+
+}
