5f273cd7598fc3605757922308d4ac85e32530fe
webbdong <wangwebb1991@gmail.com>
2021-04-20 11:38:09 +0800

    HotLoaderByByteBuddy

---
+++ advanced-classloader/src/main/java/com/webbdong/classloader/hotloadandunload/hotload/HotLoaderByByteBuddy.java
@@ -0,0 +1,40 @@
+package com.webbdong.classloader.hotloadandunload.hotload;
+
+import com.webbdong.classloader.hotloadandunload.TestClass;
+import com.webbdong.classloader.util.ByteCodeUtils;
+import lombok.SneakyThrows;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @author Webb Dong
+ * @date 2021-04-20 1:56 AM
+ */
+public class HotLoaderByByteBuddy {
+
+    @SneakyThrows
+    public static Class<?> replaceClass(String basePath, String className) {
+        String classPath = new StringBuilder()
+                .append(basePath)
+                .append(className.replaceAll("\\.", "/"))
+                .append(".class")
+                .toString();
+        try (InputStream in = new FileInputStream(classPath)) {
+            if (in == null) {
+                throw new ClassNotFoundException("class not found " + className);
+            }
+
+            byte[] bytes = ByteCodeUtils.readLocalClassAsBytes(in);
+            return new ByteBuddy()
+                    .redefine(TestClass.class, ClassFileLocator.Simple.of(className, bytes))
+                    .make()
+                    .load(HotLoaderByByteBuddy.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent())
+                    .getLoaded();
+        }
+    }
+
+}
